56e6a9d6f86e301504bff043fe4d8f8f88357076
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-09-18 11:15:59 -0300

    Criar tabelas da nossa base de dados

---
+++ src/config/database.js
@@ -0,0 +1,43 @@
+const sqlite = require("sqlite3").verbose();
+const DB_SOURCE = "src/config/database/database.sqlite";
+
+/**
+ * id
+ * first_name
+ * last_name
+ * phone
+ * email
+ * tipo de telefone
+ * endereco
+ * github
+ * linkedin
+ * pix
+ */
+const CONTACTS = `
+  CREATE TABLE IF NOT EXISTS contatos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    primeiro_nome TEXT,
+    ultimo_nome TEXT,
+    telefone TEXT,
+    tipo_telefone TEXT,
+    email TEXT,
+    endereco TEXT,
+    github TEXT,
+    linkedin TEXT,
+    pix TEXT,
+    CONSTRAINT email_unique UNIQUE(email)
+  );
+`;
+
+const database = new sqlite.Database(DB_SOURCE, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  database.run(CONTACTS, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+module.exports = database;
