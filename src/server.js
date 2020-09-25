ad3d38ef0fe879aa89c95960c9143a3de0bc59c3
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-09-24 21:41:09 -0300

    Adicionar requests para Atualizar e Deletar recursos

--- src/server.js
+++ src/server.js
@@ -123,8 +123,77 @@ app.post("/api/contatos", (req, res) => {
 });
 
 /**
+ * Atualizar um contato existente
+ */
+app.put("/api/contatos/:id", (req, res) => {
+  const {
+    body: {
+      primeiro_nome,
+      ultimo_nome,
+      telefone,
+      tipo_telefone,
+      email,
+      endereco,
+      github,
+      linkedin,
+      pix,
+    },
+  } = req;
+  const { id } = req.params;
+
+  const query = `UPDATE contatos SET
+    primeiro_nome = COALESCE(?, primeiro_nome),
+    ultimo_nome = COALESCE(?, ultimo_nome),
+    telefone = COALESCE(?, telefone),
+    tipo_telefone = COALESCE(?, tipo_telefone),
+    email = COALESCE(?, email),
+    endereco = COALESCE(?, endereco),
+    github = COALESCE(?, github),
+    linkedin = COALESCE(?, linkedin),
+    pix = COALESCE(?, pix)
+    WHERE id = ?
+  `;
+
+  const params = [
+    primeiro_nome,
+    ultimo_nome,
+    telefone,
+    tipo_telefone,
+    email,
+    endereco,
+    github,
+    linkedin,
+    pix,
+    id,
+  ];
+
+  database.run(query, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.json({
+      message: "success",
+      data: { updated_rows: this.changes },
+    });
+  });
+});
+
+/**
  * Deletar um contato
  */
-app.delete("/api/contatos/:id", (req, res) => {});
+app.delete("/api/contatos/:id", (req, res) => {
+  const query = "DELETE FROM contatos WHERE id = ?";
+  const params = [req.params.id];
+
+  database.run(query, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    console.log(result);
+    res.json({
+      message: "success",
+    });
+  });
+});
 
 module.exports = { app };
